522264fabd687422bf4a63d2908166750b81f34a
Shivam Chaurasia <chaurasias431@gmail.com>
2026-07-27 12:22:29 +0530

    Added Calculator Project

---
+++ main.py
@@ -0,0 +1,7 @@
+num1 = float(input("enter first number: "))
+num2 = float(input("enter secind number: "))
+
+print("Addition =", num1 + num2)
+print("Subtraction =", num1 - num2)
+print("multiplication =", num1 * num2)
+print("Division =", num1 / num2)
